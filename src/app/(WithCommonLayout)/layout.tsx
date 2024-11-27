97df93024b0e5b591f833b8603de0982f2f8074b
rabby <rabby.65301@gmail.com>
2024-11-27 15:51:47 +0600

    initial project setup

---
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function CommonLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+
+        <div>
+            {children}
+        </div>
+
+    );
+}
