91ce8a8cbbef3ebf8741a39e3e79a46d7211c1af
rabby <rabby.65301@gmail.com>
2024-11-28 23:28:57 +0600

    added university Api

---
+++ src/lib/redux/features/university/universityApi.ts
@@ -0,0 +1,42 @@
+import { baseApi } from "../../api/baseApi";
+
+
+const universityApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+
+        getAllUinversity: builder.query({
+            query: () => {
+                return {
+                    url: "/university",
+                    method: "GET",
+
+                }
+            }
+        }),
+
+        getUinversity: builder.query({
+            query: (id) => {
+                return {
+                    url: `/university/${id}`,
+                    method: "GET",
+
+                }
+            }
+        }),
+
+        addUinversity: builder.mutation({
+            query: (data: FormData) => ({
+                url: "/register/agent",
+                method: "POST",
+                body: data,
+
+            })
+        })
+
+
+    })
+})
+
+
+
+export const { useGetAllUinversityQuery, useGetUinversityQuery, useAddUinversityMutation } = universityApi;
